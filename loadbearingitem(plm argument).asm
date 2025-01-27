e62228f58bbddfae8ead2ea3a4bc8e00e2f45367
n-neen <115341144+n-neen@users.noreply.github.com>
2025-01-26 21:16:17 -0500

    Add files via upload

---
+++ loadbearingitem(plm argument).asm
@@ -0,0 +1,41 @@
+;this version checks for the plm's tile to match bitmask of plm's argument
+
+lorom
+
+org $848116
+    plm:
+        dw #.init, #.inst
+        
+        .init:
+            rts
+            
+        .inst:
+            dw $86c1, #.check           ;set preinstruction to our routine
+            dw $86b4                    ;sleep
+            
+        .check:
+            phx
+            phy
+            
+            ldy $1c27                   ;plm index
+            lda $1dc7,y
+            sta $16                     ;back up plm argument
+            ldx $1c87,y                 ;plm block index
+            lda $7f0002,x               ;level data word for the tile where the plm is
+            cmp $16                     ;check [plm room argument]
+            bne +                       ;if not matching, exit
+            
+            lda #$ffff                  ;if matching,
+            sta $1840                   ;set earthquake timer
+            lda #$c131
+            sta $07df                   ;set main room routine pointer to generate explosions
+            tyx
+            stz $1c37,x                 ;delete plm
+            
++           ply
+            plx
+            
+            rts
+            
+    
+    ;warn pc ;8232 is end of free space
